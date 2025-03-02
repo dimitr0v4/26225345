1673ba07fbf04a8ef7e8fbda960c85ccf1a87031
dimitr0v4 <coolasallhel@gmail.com>
2025-03-02 15:14:12 +0200

    Update Program.cs

--- 262255/Program.cs
+++ 262255/Program.cs
@@ -5,6 +5,10 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+             Univerity test=new University {new Student("georgi",360),new Student("mila",400),new Student("alex",300) };
+            Student s=new Student("dimitar",370);
+            test.AddStudent(s);
+            Console.WriteLine(test.FindByScore(360));
         }
     }
 }
