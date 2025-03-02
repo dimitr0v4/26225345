09ef6dda46b90e71c249eedc36acac1b9b4f1b97
dimitr0v4 <coolasallhel@gmail.com>
2025-03-02 15:15:31 +0200

    Update University.cs

--- 262255/University.cs
+++ 262255/University.cs
@@ -13,7 +13,28 @@ namespace _262255
             { uni.Add(student);
             InsSort(uni);}
         public static void InsSort(List<Student> uni) //insertion sort adjusted
-            { }
-        public void FindStByScore(int score) { }//dwoichno
+            { int n=uni.Length;
+
+             for(int i=1;i<n;i++)
+                {Student keyS=uni[i];
+                int key=uni[i].Score;//name&score
+                    int j=i-1;
+                    while(j>=0&&uni[j].Score>key)
+                        {uni[j+1]=uni[j];
+                            j--;}
+                        uni[j+1]=keyS;}}
+        public void FindStByScore(int target) {
+        int left=0;
+            int right=array.Length-1;
+            while(left<=right)
+                {int mid=left+(right-left)/2;
+                    if(uni[mid].Score==target)
+                        {return mid;}
+                    else if(uni[mid].Score<target)
+                        {let=mid+1;}
+                    else
+                        {right=mid-1;}
+                 }
+            return -1; }//dwoichno
     }
 }
