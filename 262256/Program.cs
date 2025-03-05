d8e67ff9bd817cc714342a920e3c74c19333fb32
dimitr0v4 <coolasallhel@gmail.com>
2025-03-05 10:08:15 +0200

    Update Program.cs

--- 262256/Program.cs
+++ 262256/Program.cs
@@ -5,6 +5,11 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+             Company c = new Company ();
+            c.firm.Add(new Employee("georgi",3000));
+            c.firm.Add(new Employee("mila", 3001));
+            c.firm.Add(new Employee("alex", 3002));
+            Console.WriteLine(c.FindEmployeeBySalary(3002));
         }
     }
 }
