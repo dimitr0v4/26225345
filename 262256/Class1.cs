3a880388d36e801e44a2257138f16f41272d90f7
dimitr0v4 <coolasallhel@gmail.com>
2025-03-05 10:09:51 +0200

    Add files via upload

---
+++ 262256/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Employee
+    {public string Name { get; set; }
+        public int Salary { get; set;}
+        public Employee(string name,int slary) { Name = name; Salary = slary; }
+    }
+}
---
+++ 262256/Company.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+namespace ConsoleApp2
+{
+    class Company
+    {
+       public List<Employee> firm = new List<Employee> { };
+        public int FindEmployeeBySalary(int salary)
+        {
+            for (int i = 0; i < firm.Count; i++)
+            {
+                if (firm[i].Salary == salary) { return i; }
+            }
+            Console.WriteLine("Не е намерен");
+            return -1;
+        }//linejno ""
+    }
+}
